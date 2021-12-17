994fad90811bc615b547f716ff5d2361f8c3c576
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-12-17 17:08:37 -0600

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,17 +8,8 @@ const styles = {
 
 function Footer() {
   return (
-    <footer id="contact" className="text-center" style={styles.footer}>
-      <h2>Contact Me</h2>
+    <footer id="contact" className="footer text-center" style={styles.footer}>
       <ul className="pb-3 pl-1">
-        <li>
-          <span className="fa fa-phone"></span>
-          <a href="#">(210)630-2578</a>
-        </li>
-        <li>
-          <span className="fa fa-paper-plane"></span>
-          <a href="#">mvallejo14957@yahoo.com</a>
-        </li>
         <li>
           <span className="fa fa-github"></span>
           <a href="https://github.com/Mvalljo">Github</a>
@@ -26,7 +17,6 @@ function Footer() {
         <li>
           <span className="fa fa-linkedin-square"></span>
           <a href="https://www.linkedin.com/in/maria-vallejo-8237a41a6/">
-            Linkdln
           </a>
         </li>
       </ul>
