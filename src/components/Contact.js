d27acd23f6ebae2abe2073ae1784e37e1a1027ef
Maria Vallejo <maria.vallejo29@yahoo.com>
2022-02-03 14:29:52 -0600

    set up send a msg in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiPhone } from "react-icons/fi";
 import { HiOutlineMail } from "react-icons/hi";
 
@@ -9,6 +9,27 @@ const styles = {
 };
 
 function AboutMe() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const submitRequest = async (e) => {
+    e.preventDefault();
+    console.log({ email, message });
+    const response = await fetch("/access", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({ email, message })
+    });
+    const resData = await response.json();
+    if (resData.status === "success") {
+      alert("Message Sent.");
+      this.resetForm();
+    } else if (resData.status === "fail") {
+      alert("Message failed to send.");
+    }
+  };
   return (
     <div className="py-5 container">
       <div className="row py-lg-5">
@@ -25,6 +46,39 @@ function AboutMe() {
             <HiOutlineMail fontSize="30px" className="m-3" />
             mvallejo14957@yahoo.com
           </p>
+          <form onSubmit={submitRequest}>
+            <h2 className="text-2xl pt-6 pb-10 text-center font-medium">
+              Send a Message
+            </h2>
+            <div className="d-flex flex-column">
+              <label className="mb-2" htmlFor="Email">
+                Email:
+              </label>
+              <input
+                type="text"
+                name="email"
+                placeholder="Email Address"
+                className="mb-4 text-dark"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+              />
+              <label className="mb-2" htmlFor="message">
+                Message
+              </label>
+              <textarea
+                name="message"
+                type="text"
+                className="mb-4 text-dark"
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+                required
+              />
+              <button className="bg-primary rounded mb-2" type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
